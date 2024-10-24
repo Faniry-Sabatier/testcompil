b771351ecc91657fb1e1d8fd8bafbd8d739da50e
Sabatier <69795003+Faniry-Sabatier@users.noreply.github.com>
2024-10-24 10:58:05 +0300

    Create c00.c

---
+++ c00.c
@@ -0,0 +1,75 @@
+EX00
+
+int	main()
+{
+	ft_putchar('m');
+	return(0);
+}
+
+EX01
+
+int	main()
+{
+	ft_print_alphabet();
+	return (0);
+}
+
+EX02
+
+int	main()
+{
+	ft_print_reverse_alphabet();
+}
+
+EX03
+
+int	main()
+{
+	ft_print_numbers();
+}
+
+EX04
+
+int	main()
+{
+	ft_is_negative(-1);
+}
+
+EX05
+
+int	main()
+{
+	ft_print_comb();
+	return(0);
+}
+
+EX06
+
+int main ()
+{
+	ft_print_comb2();
+} 
+
+EX07
+
+int main (void)
+{
+	ft_putnbr(4563);
+	ft_putchar('\n');
+	ft_putnbr(-2147483648);
+	ft_putchar('\n');
+	ft_putnbr(42);
+	ft_putchar('\n');
+	ft_putnbr(2147483647);
+	ft_putchar('\n');
+	ft_putnbr(5);
+	ft_putchar('\n');
+
+}
+
+EX08
+
+int	main()
+{
+	ft_print_combn(0);
+} 
